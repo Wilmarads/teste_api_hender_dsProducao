63e7450ccc3903aeb43a2ada516120a090998961
Wilmarads <wilmara.as@gmail.com>
2023-12-28 11:56:48 -0300

    correction of os.eviron attribute to os.environ

--- handler.py
+++ handler.py
@@ -48,7 +48,7 @@ def rossmann_predict():
         return Reponse( '{}', status=200, mimetype='application/json' )
 
 if __name__ == '__main__':
-    port = os.eviron.get( 'PORT', 5000 )
+    port = os.environ.get( 'PORT', 5000 )
     app.run( host='0.0.0.0', port=port )
 
 
